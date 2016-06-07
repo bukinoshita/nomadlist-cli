4547d769b5ada47f7b7163b144e989568d527822
Bu Kinoshita <bukinoshita@gmail.com>
2016-06-07 15:38:57 -0400

    Refactoring

--- nomadlist.js
+++ nomadlist.js
@@ -6,6 +6,7 @@ const program = require('commander');
 const pkg = require('./package.json');
 const version = pkg.version;
 const request = require('request');
+const got = require('got');
 const spinner = require('./src/spinner');
 const args = process.argv.slice(2);
 
@@ -17,75 +18,60 @@ program
   .option('taylor', 'Taylor bot')
   .parse(process.argv);
 
-const getMethod = (argv) => {
+const api = (argv) => {
   if(!argv[0]) {
     program.help();
-  } else if(argv == 'cities') {
-    const url = 'https://nomadlist.com/api/v2/list/cities';
-
-    console.log('Getting cities...');
+  } else if(program.cities) {
     spinner.start();
 
-    request({
-    	method: 'GET',
-    	url: url
-    }, function(error, response, body) {
-      var body = JSON.parse(body);
+    got('https://nomadlist.com/api/v2/list/cities')
+      .then(response => {
+        spinner.stop();
+        var cityList = JSON.parse(response.body);
 
-      for(var i = 0; i < body.result.length; i++) {
-        var nomadlist = body.result[i];
+        for(var i = 0; i < cityList.result.length; i++) {
+          var nomadlist = cityList.result[i];
 
-        console.log('City: ', nomadlist.info.city.name);
-        console.log('Country: ', nomadlist.info.country.name);
-        console.log('Cost: ', '$' + nomadlist.cost.nomad.USD + '/m');
-        console.log('Internet: ', nomadlist.info.internet.speed.download + ' MBPS');
-        console.log('Weather: ', nomadlist.info.weather.type + ' — ' + nomadlist.info.weather.temperature.celsius + 'C');
-        console.log('\r\n');
-      }
+          console.log('City: ', nomadlist.info.city.name);
+          console.log('Country: ', nomadlist.info.country.name);
+          console.log('Cost: ', '$' + nomadlist.cost.nomad.USD + '/m');
+          console.log('Internet: ', nomadlist.info.internet.speed.download + ' MBPS');
+          console.log('Weather: ', nomadlist.info.weather.type + ' — ' + nomadlist.info.weather.temperature.celsius + 'C');
+          console.log('\r\n');
+        }
 
-      process.exit(0);
-    });
-  } else if(argv == 'remoteok') {
-    const url = 'https://remoteok.io/index.json';
-
-    console.log('Getting remote jobs...');
+        process.exit(0);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  } else if(program.remoteok) {
     spinner.start();
 
-    request({
-    	method: 'GET',
-    	url: url
-    }, function(error, response, body) {
-      var body = JSON.parse(body);
+    got('https://remoteok.io/index.json')
+      .then(response => {
+        spinner.stop();
 
-      for(var i = 0; i < body.length; i++) {
-        var nomadlist = body[i];
+        var jobList = JSON.parse(response.body);
 
-        console.log('Date: ', nomadlist.date);
-        console.log('Company: ', nomadlist.company);
-        console.log('Position: ', nomadlist.position);
-        console.log('URL: ', nomadlist.url);
-        console.log('\r\n');
-      }
+        for(var i = 0; i < jobList.length; i++) {
+          var nomadlist = jobList[i];
 
-      process.exit(0);
-    });
+          console.log('Date: ', nomadlist.date);
+          console.log('Company: ', nomadlist.company);
+          console.log('Position: ', nomadlist.position);
+          console.log('URL: ', nomadlist.url);
+          console.log('\r\n');
+        }
+
+        process.exit(0);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   return argv[0];
 }
 
-const taylor = () => {
-  console.log('Loading places to taylor...');
-}
-
-const method = getMethod(args);
-
-try {
-  switch (method) {
-    case 'taylor':
-      taylor();
-      break;
-  }
-} catch(e) {
-  program.help();
-}
+api(args);
